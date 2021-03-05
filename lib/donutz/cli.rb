4a2788f81049d0b863dd86e2113b1d7d50aeec1a
Matt Smith <mrandolph83@gmail.com>
2021-03-05 18:23:18 +0000

    Changed @donut_selection to array to make qty an instance variable

--- lib/donutz/cli.rb
+++ lib/donutz/cli.rb
@@ -1,9 +1,10 @@
 require 'pry'
 class Donutz::CLI 
+  
   def call
     new_order
     get_menu
-    donut_menu
+    donut_menu_first_purchase
   end
      
   def new_order
@@ -18,43 +19,69 @@ class Donutz::CLI
     puts "Thanks #{@name}! Please type in your selection using the numbered list below:"
     puts "The red numbers indicate how many donuts are currently in stock."
     puts ""
-    @donut_selection = {"Glazed" => 36, "Chocolate Sprinkles" => 16, "Rainbow Sprinkes" => 42, "Strawberry Glaze" => 22} 
+    @donut_selection = ["Glazed", "Chocolate Sprinkles", "Rainbow Sprinkes", "Strawberry Glaze"]
+    # Needs scraped for actual KK data
   end
   
-  def donut_menu
-    @donut_selection.each.with_index(1) {|(donut, qty), index|
-     puts "#{index}: #{donut} - #{qty}"
+  def donut_menu_first_purchase
+    @donut_selection.each.with_index(1) {|donut, index|
+     puts "#{index}: #{donut} - 24"
     }
-    donut_selection
+    make_selection
   end
   
-  def donut_selection
+    def donut_menu_later_purchases
+    puts "What else would you like to add to your order?"
+    @donut_selection.each.with_index(1) {|donut, index|
+     puts "#{index}: #{donut} - 24"
+    }
+    make_selection
+  end
+  
+  def make_selection
     input = gets.strip.to_i
-    puts ""
-    puts "Yay you did it!"
-    qty_selection
+    qty_selection(input) if valid_selection(input, @donut_selection) 
   end
+ 
   
-  def qty_selection
+  def qty_selection(input)
+    selected_donut = @donut_selection[input-1]
     puts ""
-    puts "Please enter how many GLAZED donuts you would like." 
+    puts "Please enter how many #{selected_donut} donuts you would like." 
     puts "Type 'm' for more information about this donut."
     input = gets.strip
     purchase_qty
-    # How do I tie in their order selection with this statement?
-    # if input = "m"
-    # donut_information(input)
-    # else
-    # purchase_qty(input.to_i)
-    # end
+end
+
+# binding.pry
+  
+  def valid_selection(input, data)
+    input.to_i <= data.length && input.to_i > 0
   end
     
   def purchase_qty
-    # Add input variable later
-   puts "Does this complete your order? (Y/N)"   
-   input = gets.strip
-   donut_menu
+    #add_to_order
+   puts "Does this complete your order? (Y/N)" 
+   puts ""
+   input = gets.strip.upcase
+    if input == "Y"
+      complete_order
+    else
+      donut_menu_later_purchases
+    end
   end
   
-  # binding.pryss
+  # def add_to_order
+  #   if amount <= qty left, remove that amount from updated menu selection.
+  #   else "There are not enough GLAZED donuts to meet your request. Please enter a new number"
+  #   end
+  # end
+  
+  def complete_order
+   puts "Thank you #{@name}! Your order is listed below. Press [p] to print." 
+   puts ""
+   puts "Glazed - $12"
+   puts "Chocolate Sprinkes - $13"
+  end
+
 end
