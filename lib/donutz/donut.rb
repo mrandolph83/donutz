d348e4ddb600cd7b3cd610119c48e9fc48778099
Matt Smith <mrandolph83@gmail.com>
2021-03-08 04:08:04 +0000

    Add initialize method

--- lib/donutz/donut.rb
+++ lib/donutz/donut.rb
@@ -1,6 +1,13 @@
 class Donutz::Donut
   
+  attr_accessor :name, :qty
+  
   @@all = ["Strawberry", "Vanilla", "Swirl"]
+  
+  def initialize(name)
+  # Set quantity starting at 36 if no qty is given 
+  @name = name
+  end
   
   def self.all
     @@all
