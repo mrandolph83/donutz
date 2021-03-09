c6e79c3c62a7e3d22bcaab30cd0af6dfc69ec97b
Matt Smith <mrandolph83@gmail.com>
2021-03-09 16:13:07 +0000

    Successful scrape of donut menu

--- lib/donutz/scraper.rb
+++ lib/donutz/scraper.rb
@@ -2,9 +2,13 @@ require 'pry'
 class Donutz::Scraper
   
   def self.scrape_menu
-    doc = Nokogiri::HTML(open("https://shipleydonuts.com/menu/"))
-    donuts = doc.css("h4.title")
+    doc = Nokogiri::HTML(open("https://www.toppotdoughnuts.com/pages/doughnuts"))
+    donuts = doc.css("button")
+    donuts.each do |each_donut|
+      puts each_donut.text
+    end
     binding.pry
+    
   end
   
 end 
