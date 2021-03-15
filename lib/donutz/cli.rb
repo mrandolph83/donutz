03da72c7c1c53b87431416457ab29ad99c8acd0a
Matt Smith <mrandolph83@gmail.com>
2021-03-15 05:35:48 +0000

    Colorize and ASCII elements

--- lib/donutz/cli.rb
+++ lib/donutz/cli.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'colorize'
 class Donutz::CLI 
   
   
@@ -10,38 +11,43 @@ class Donutz::CLI
   end
      
   def new_order
+    pink_donutz = "DONUTZ".colorize(:light_magenta)
+    yellow_name = "Name".colorize(:yellow)
     puts "
-──────────────────────────────
-────────▄████████████▄────────
-─────▄███▀░░░░░░░░░░▀███▄─────
-────███░░░░░░░░░░░░░░░░███────
-───██░░░░░░░░░░░░░░░░░░░░██───
-──██░░░░░░░░░░░░░░░░░░░░░░██──
-─██░░░░░░░░░░░░░░░░░░░░░░░░██─
+
+         ▄████████████▄
+     ▄███▀░░░░░░░░░░▀███▄
+    ███░░░░░░░░░░░░░░░░███
+   ██░░░░░░░░░░░░░░░░░░░░██
+  ██░░░░░░░░░░░░░░░░░░░░░░██
+ ██░░░░░░░░░░░░░░░░░░░░░░░░██
 ██░░░░░░░░▄████████▄░░░░░░░░██
-██░░░░░░░███▀────▀███░░░░░░░██
-██░░░░░░░██────────██░░░░░░░██
-██░░░░░░░███▄────▄███░░░░░░░██
+██░░░░░░░███▀    ▀███░░░░░░░██
+██░░░░░░░██        ██░░░░░░░██
+██░░░░░░░███▄    ▄███░░░░░░░██
 ██░░░░░░░░▀████████▀░░░░░░░░██
-─██░░░░░░░░░░░░░░░░░░░░░░░░██─
-──██░░░░░░░░░░░░░░░░░░░░░░██──
-───██░░░░░░░░░░░░░░░░░░░░██───
-────███░░░░░░░░░░░░░░░░███────
-─────▀███▄░░░░░░░░░░▄███▀─────
-────────▀████████████▀────────.colorize(:cyan)"
-    puts "Welcome to the Donutz ordering app! Please enter a name for your order:"
+ ██░░░░░░░░░░░░░░░░░░░░░░░░██
+  ██░░░░░░░░░░░░░░░░░░░░░░██
+   ██░░░░░░░░░░░░░░░░░░░░██
+   ███░░░░░░░░░░░░░░░░███
+    ▀███▄░░░░░░░░░░▄███▀
+       ▀████████████▀".colorize(:light_magenta)
+    
+    puts "\nWelcome to the #{pink_donutz} ordering app!\nPlease enter a #{yellow_name} for your order:"
     puts ""
     @order_name = gets.strip
-    name = @order_name
-    @order = Donutz::Order.new(name) # CHANGED THIS 
+    name_entered = @order_name
+    @order = Donutz::Order.new(name_entered) # CHANGED THIS 
     # instantiates new :order_name in Transaction class. Make name colorized
   end
   
   def get_menu
     @donut_selection = Donutz::Donut.all
-    puts ""
-    puts "Thanks #{@order_name}! Please type in your selection (1-#{@donut_selection.length}) using the numbered list below:"
-    puts "The red numbers indicate how many donuts are currently in stock."
+    pink_name = @order_name.upcase.colorize(:light_magenta)
+    yellow_selection = "(1-#{@donut_selection.length})".colorize(:yellow)
+    yellow_text = "yellow".colorize(:yellow)
+    puts "\nThanks #{pink_name}! Please type in your selection #{yellow_selection} using the numbered list below:"
+    puts "The #{yellow_text} numbers indicate how many donuts are currently in stock."
     puts ""
     # binding.pry
   end
@@ -49,16 +55,19 @@ class Donutz::CLI
   def donut_menu_first_purchase
     puts ""
     @donut_selection.each.with_index(1) {|(donut, qty), index|
-     puts "#{index}: #{donut.name} - #{donut.qty}"
+    print "#{index} ".colorize(:light_magenta) 
+    print "#{donut.name} - "
+    print "#{donut.qty}\n".colorize(:yellow)
     }
     make_selection
   end
   
     def donut_menu_later_purchases
-    puts "What else would you like to add to your order?"
-    puts ""
+    puts "What else would you like to add to your order?\n"
     @donut_selection.each.with_index(1) {|(donut, qty), index|
-     puts "#{index}: #{donut.name} - #{donut.qty}"
+    print "#{index} ".colorize(:light_magenta) 
+    print "#{donut.name} - "
+    print "#{donut.qty}\n".colorize(:yellow)
     }
     make_selection
   end
@@ -74,7 +83,8 @@ class Donutz::CLI
    if input.between?(1, last_menu_item)
     qty_selection(input)
    else
-    puts "Please enter a valid number (1-#{@donut_selection.length}) to continue your order."
+    yellow_selection = "(1-#{@donut_selection.length})".colorize(:yellow) 
+    puts "Please enter a valid number #{yellow_selection} to continue your order."
      make_selection
     end
   end
@@ -82,9 +92,10 @@ class Donutz::CLI
   
   def qty_selection(input)
     selected_donut = @donut_selection[input-1]
-    puts ""
-    puts "Please enter how many #{selected_donut.name} donuts you would like." 
-    puts "Type 'i' for more information about this donut."
+    pink_donut = selected_donut.name.upcase.colorize(:light_magenta)
+    yellow_i = "'i'".colorize(:yellow)
+    puts "\nPlease enter how many #{pink_donut} donuts you would like." 
+    puts "Type #{yellow_i} for more information about this donut."
     puts ""
     qty = gets.strip
     # if valid_qty(qty)
@@ -100,15 +111,28 @@ class Donutz::CLI
   
   def donut_information(selected_donut)
     selected_info = Donutz::Scraper.scrape_info(selected_donut)
+    yellow_p = "'p'".colorize(:yellow)
+    yellow_n = "'n'".colorize(:yellow)
+    yellow_enter = "'Enter'".colorize(:yellow)
     puts ""
-    puts "Awesome picture"
+    puts selected_donut.name.upcase.colorize(:light_magenta)
     # @donut_selection.pic. I'm guessing the object is pic url
-    puts "#{selected_info}"
-    puts ""
-    puts "Press the ENTER key to return to the Order Menu."
+    puts "\n#{selected_info}\n"
+    puts "\nPress #{yellow_p} to open a picture of this donught in your browser,"
+    puts "or #{yellow_n} to open nutrition facts."
+    puts "\nPress the #{yellow_enter} key to return to the Order Menu."
     input = gets.strip
-    donut_menu_later_purchases if input == ""
+    if input == ""
+      donut_menu_later_purchases
+    elsif input.upcase == "P"
+      puts "Open Me Daddy"
+      donut_information(selected_donut)
+    else input.upcase == "N"
+      puts "This is terrible for you, you fool"
+      donut_information(selected_donut)
+    end
   end
+  
   
   def valid_qty(qty, selected_donut)
     qty.between?(1, selected_donut.qty)
@@ -139,6 +163,23 @@ class Donutz::CLI
    puts donuts.name.upcase
    puts donuts.order_items
    }
+   puts "
+   
+         d888888b                         d888888b
+   d888    8888b                    d888888   888b
+ d88    88  898888b               d8888  888     88b
+d8P        88888888b             d88888888888     b8b
+88        8888888888             88888888888       88
+88       88888888888             8888888888        88
+98b     88888888888P             988888888        d8P
+ 988     888  8888P      _=_      9888898  88    88P
+   9888   888888P      q(-_-)p       98888    888P
+      9888888P         '_) (_`         9888888P
+         88            /__/  \            88
+         88          _(<_   / )_          88
+        d88b        (__\_\_|_/__)        d88b".colorize(:light_magenta)
+   quote_selection = Donutz::Scraper.scrape_quotes 
+   puts "'#{quote_selection}'".colorize(:yellow)
   # binding.pry
   # Time.now stamp
   end
--- lib/donutz/scraper.rb
+++ lib/donutz/scraper.rb
@@ -1,10 +1,19 @@
 require 'pry'
 class Donutz::Scraper
   
+  def self.scrape_quotes
+      doc = Nokogiri::HTML(open("https://sayingimages.com/donut-quotes/"))
+      donut_quotes = []
+      doc.css("em").each do |quotes|
+      donut_quotes << quotes.text
+   end
+      donut_quotes.sample
+      
+  end
+  
   def self.scrape_menu
       doc = Nokogiri::HTML(open("https://shipleydonuts.com/menu/"))
       doc.css("h4 > a").each do |donut_traits|
- 
       name = donut_traits.text
       qty = 36
       url = donut_traits.attr("href")
